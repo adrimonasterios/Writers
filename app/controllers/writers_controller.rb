b65c674f089db6aac0d5aafd3746c38affc9e0bf
Adrian <adri.suscripciones@gmail.com>
2018-12-29 01:55:51 -0500

    writers links set

--- app/controllers/writers_controller.rb
+++ app/controllers/writers_controller.rb
@@ -4,4 +4,10 @@ class WritersController < ApplicationController
     @writers = Writer.all
   end
 
+  def show
+    @writer_id = params[:id]
+    @current_writer = Writer.where(id: @writer_id).first
+    @books = Book.where(writer_id: @writer_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'writers#index'
 
   resources :books, only: [:index]
+  resources :writers, only: [:index, :show]
 end
