87811211106e07a0793a2991f97bdf1969d11a71
Adrian <adri.suscripciones@gmail.com>
2019-01-04 02:57:23 -0500

    all routes working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,10 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def show
+    @book_id = params[:id]
+    @current_book = Book.where(id: @book_id).first
+    @book_author = Writer.where(id: @current_book.writer_id).first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'writers#index'
 
-  resources :books, only: [:index]
+  resources :books, only: [:index, :show]
   resources :writers, only: [:index, :show]
 end
