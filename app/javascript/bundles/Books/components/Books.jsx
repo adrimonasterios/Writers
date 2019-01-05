8265eb202b36b8d362a220ce6171faa4eeb26ce8
Adrian <adri.suscripciones@gmail.com>
2019-01-05 17:49:44 -0500

    horizontal alphabet

--- app/javascript/bundles/Books/components/Books.jsx
+++ app/javascript/bundles/Books/components/Books.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 
 class Books extends React.Component{
+  state={
+    letter: 'A'
+  }
+
   render(){
     return(
       <div>
-        {this.props.alphabet.map((element)=>{
-          return <p>{element}</p>
-        })}
-        {this.props.books.map((element, index)=>{
-          let {writers} = this.props
-          let authorId = element.writer_id
-          let author = ""
+        <div className="alphabet">
+          {this.props.alphabet.map((element)=>{
+            return <p className="letter" onClick={(ev)=>{this.setState({letter: element})}}>{element}</p>
+          })}
+        </div>
+        <div>
+          {this.props.books.map((element, index)=>{
+            let {writers} = this.props
+            let authorId = element.writer_id
+            let author = ""
 
-          for(let i=0; i<writers.length; i++){
-            if(authorId == writers[i].id){
-              author  = `${writers[i].name} ${writers[i].last_name}`
+            for(let i=0; i<writers.length; i++){
+              if(authorId == writers[i].id){
+                author  = `${writers[i].name} ${writers[i].last_name}`
+              }
             }
-          }
-          return <p>{element.name} (by {author})</p>
-        })}
+            if(this.state.letter == element.name[0]){
+              return <p>{element.name} (by {author})</p>
+            }
+          })}
+        </div>
       </div>
     )
   }
